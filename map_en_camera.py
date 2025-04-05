0ff3cdc41fd026af3b62a6a4ec88078813e31453
Wouter Gobeyn <woutergobeyn@gmail.com>
2025-04-05 16:32:40 +0200

    basic gameloop class gemaakt volgens tutorial

--- map_en_camera.py
+++ map_en_camera.py
@@ -1,1 +1,1 @@
-#gevolgde tutotial via https://www.youtube.com/watch?v=3UxnelT9aCo&list=PLsk-HSGFjnaGQq7ybM8Lgkh5EMxUWPm2i
+#gevolgde tutotials via https://www.youtube.com/watch?v=3UxnelT9aCo&list=PLsk-HSGFjnaGQq7ybM8Lgkh5EMxUWPm2i
--- project.py
+++ project.py
@@ -1,3 +1,64 @@
-#test
-print("test geslaagd")
-print('fuckass programma')
+#tips voor elkaar:
+#om alle instances van een bepaalde term te selecteren en tegelijk te kunnen veranderen, selecteer woord en druk op ctrl+shift+L
+#in pg.ingebouwde sprite basisklasse mag niet gebruitkt worden
+
+import pygame as pg
+from GameSettings import * #importeer alle setting zonder steeds GameSettings."setting" te moeten typen
+
+class Game:
+    def __init__(self):
+        # startup van game window, etc 
+        pg.init()
+        #pg.mixer.init() #initieert geluidsfunctie van pg.(niet gebruikt in project)
+        self.screen = pg.display.set_mode((BREEDTE,HOOGTE))
+        pg.display.set_caption(TITEL)
+        self.clock = pg.time.Clock()
+        self.running = True
+    
+    def new(self):
+        #start nieuwe game
+        pass
+
+    def run(self):
+        # game loop
+        self.playing = True
+        while self.playing:
+            self.clock.tick(FPS)
+            self.events()
+            self.update()
+            self.draw()
+    
+    def update(self):
+        #update game loop
+        pass
+
+    def events(self):
+        #events binnen game loop
+        for event in pg.event.get():
+        #check voor game afsluiten met X van de gamewindow
+            if event.type == pg.QUIT:
+                self.playing = False
+                self.running = False
+                
+
+    def draw(self):
+        # teken frame
+        self.screen.fill(ZWART)
+        #altijd laatste line van renderen
+        pg.display.flip() 
+
+    def toon_startscherm(self):
+        pass
+
+    def game_over(self):
+        pass
+
+#uit te voeren code
+game = Game()
+game.toon_startscherm()
+while game.running:
+    game.new()
+    game.run()
+    game.game_over()
+
+pg.quit()
