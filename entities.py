a7497ffaee160a55200820d84282587a1e3ac753
Wouter Gobeyn <woutergobeyn@gmail.com>
2025-04-05 18:57:13 +0200

    Basische playermovement geïmplementeerd

--- entities.py
+++ entities.py
@@ -1,1 +1,24 @@
+import pygame as pg
+from GameSettings import *
+
+entitylijst = []
+
 #maak hier entity classes
+class Player():
+    def __init__(self,game,x,y, kleur): #gebruik relatieve posities zodat de player altijd binnen squares blijft
+        self.game = game
+        self.x = x
+        self.y = y
+        self.kleur = kleur
+
+    def move(self, dx=0, dy=0): #beweging met standaardhoeveelheid 0 zodat er standaard geen beweging is
+        self.x += dx
+        self.y += dy
+
+    def update(self):
+        self.rect_x = self.x * TILESIZE
+        self.rect_y = self.y * TILESIZE
+
+    #zelf tekenen is nodig omdat we geen sprite super-class mogen gebruiken    
+    def draw(self):
+        pg.draw.rect(self.game.screen, self.kleur,(self.rect_x,self.rect_y,TILESIZE,TILESIZE)) 
--- project.py
+++ project.py
@@ -1,9 +1,11 @@
 #tips voor elkaar:
 #om alle instances van een bepaalde term te selecteren en tegelijk te kunnen veranderen, selecteer woord en druk op ctrl+shift+L
 #in pg.ingebouwde sprite basisklasse mag niet gebruitkt worden
+#save altijd een file na het aanmaken van een nieuwe klasse, ander krijg je "niet defined" error bij het starten van programma
 
 import pygame as pg
 from GameSettings import * #importeer alle setting zonder steeds GameSettings."setting" te moeten typen
+from entities import *
 
 class Game:
     def __init__(self):
@@ -13,11 +15,13 @@ class Game:
         self.screen = pg.display.set_mode((BREEDTE,HOOGTE))
         pg.display.set_caption(TITEL)
         self.clock = pg.time.Clock()
+        pg.key.set_repeat(30,30)
         self.running = True
     
     def new(self):
         #start nieuwe game
-        pass
+        self.player = Player(self,10,10,GEEL) #werkt met squares als coord. , niet met pixels
+        entitylijst.append(self.player)
 
     def run(self):
         # game loop
@@ -30,15 +34,28 @@ class Game:
     
     def update(self):
         #update game loop
-        pass
+        for entity in entitylijst:
+            entity.update()
 
     def events(self):
         #events binnen game loop
         for event in pg.event.get():
-        #check voor game afsluiten met X van de gamewindow
+            #check voor game afsluiten met X van de gamewindow
             if event.type == pg.QUIT:
                 self.playing = False
                 self.running = False
+            #check key-inputs
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_ESCAPE:
+                    pg.quit()
+                if event.key == pg.K_q:
+                    self.player.move(dx=-1)
+                if event.key == pg.K_d:
+                    self.player.move(dx=1)
+                if event.key == pg.K_z:
+                    self.player.move(dy=-1)
+                if event.key == pg.K_s:
+                    self.player.move(dy=1)
     
     def teken_grid(self):
         for x in range(0, BREEDTE, TILESIZE):
@@ -51,6 +68,8 @@ class Game:
         # teken frame
         self.screen.fill(ACHTERGRONDKLEUR)
         self.teken_grid()
+        for entity in entitylijst:
+            entity.draw()
         #altijd laatste line van renderen
         pg.display.flip() 
 
