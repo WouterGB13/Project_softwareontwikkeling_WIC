7a5c3e0cb1560e6bb0d180d56efd13203f64f221
WouterGB13 <woutergobeyn@gmail.com>
2025-04-05 14:55:04 +0200

    Create GameSettings.py

---
+++ GameSettings.py
@@ -0,0 +1,11 @@
+#Game opties en settings
+BREEDTE = 360
+HOOGTE = 480
+FPS = 30
+
+#definieer nuttige kleuren
+WIT = (255,255,255)
+ZWART = (0,0,0)
+ROOD = (255,0,0)
+GROEN = (0,255,0)
+BLAUW = (0,0,255)
